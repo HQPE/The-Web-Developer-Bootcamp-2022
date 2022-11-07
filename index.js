16c89e9deb2c3cd1b50844c40634796cbcd442e9
HQPE <65927735+HQPE@users.noreply.github.com>
2022-11-07 23:01:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-// Change the value of num, so that "YOU GOT ME!" prints out
-const num = 102; // THIS IS THE ONLY LINE YOU SHOULD CHANGE :) 
+const mystery = 'PPPPP7'; //CHANGE THIS VALUE TO MAKE THE CONDITIONAL BELOW TRUE
 
-// DO NOT TOUCH ANYTHING BELOW (please) 
-if(num <= 100) {
-    if(num >= 50) {
-        console.log("HEY!");
-    }
-} else {
-    if (num < 103) {
-        if(num % 2 === 0){
-            console.log("YOU GOT ME!");
-        }
-    }
+
+
+// LEAVE THIS CODE ALONE! (pretty please)
+if(mystery[0] === 'P' && mystery.length > 5 && mystery.indexOf('7') !== -1){
+    console.log("YOU GOT IT!!!");
 }
